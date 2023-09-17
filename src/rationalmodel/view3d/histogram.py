277d5688a4aa647ad1ad1d9d2321a975b6b5ebbb
emartirequena <emartirequena@yahoo.com>
2023-09-17 18:06:55 +0200

    Add got to Init and End time line

--- src/rationalmodel/view3d/histogram.py
+++ src/rationalmodel/view3d/histogram.py
@@ -1,14 +1,145 @@
-from copy import deepcopy
-import gc
-
-from PyQt5 import QtWidgets, QtCore, QtGui
-import moderngl as mgl, glm
-from madcad import brick, vec3, Axis, X, settings, fvec3
-from madcad.rendering import View, Scene, Orthographic, Turntable
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QMouseEvent, QWheelEvent, QKeyEvent
+from PIL import Image, ImageDraw
+from madcad import vec3
+import numpy as np
 
 from config import Config
 from color import ColorLine
-from renderView import RenderView
+from utils import lerp
+
+
+class Item:
+    def __init__(self, x: int, height: int, color: vec3) -> None:
+        self.x = x
+        self.height = height
+        self.color = (
+            int(255 * color.x), 
+            int(255 * color.y), 
+            int(255 * color.z)
+        )
+
+
+class Scene:
+    def __init__(self, width: int, height: int, y_factor: float=1.) -> None:
+        self.width: int = width
+        self.height: int = height
+        self.min_x = 0.
+        self.max_x = 0.
+        self.max_h = 0.
+        self.y_factor = y_factor
+        self.ox: float = width / 2
+        self.scl: float = 1.
+        self.items: list[Item] = []
+
+    def clear(self, max_h=1.):
+        self.items = []
+        self.max_x = 0.
+        self.max_h = max_h
+
+    def add(self, x: int, height: int, color: vec3):
+        self.items.append(Item(x, height, color))
+        if x > self.max_x: self.max_x = x
+
+    def scale(self, x: float, step: float):
+        sox = self.ox * self.scl
+        factor = 1.05 if step > 0. else 1./1.05
+        self.scl *= factor
+        self.ox = ((sox - x) * factor + x) / self.scl
+
+    def fit(self):
+        self.min_x =  10000000000
+        self.max_x = -10000000000
+        for item in self.items:
+            if item.x < self.min_x: self.min_x = item.x
+            if item.x > self.max_x: self.max_x = item.x
+        if self.max_x != self.min_x:
+            self.scl = self.width / (self.max_x - self.min_x)
+            self.ox = -self.min_x
+        else:
+            self.scl = self.width / self.max_x
+            self.ox = 0.
+
+    def translate(self, dist: float):
+        self.ox += dist / self.scl
+
+    @staticmethod
+    def _loga(a, x):
+        return np.log(x) / np.log(a)
+    
+    def _loga_round(self, a, x):
+        return int(np.power(a, int(self._loga(a, x))))
+    
+    def _get_y_step_max(self, y_base):
+        y_step = int(self._loga_round(y_base, self.max_h))
+        y_step = y_step if y_step > 0 else 1
+        y_max = y_step * np.power(y_base, 2)
+        return y_step, y_max
+    
+    def _get_x_step_max(self, x_base):
+        x_step = self._loga_round(x_base, x_base * 10 / self.scl)
+        x_step = x_step if x_step > 0 else 1
+        x_max = x_step * np.power(x_base, 3)
+        return x_step, x_max
+
+    def _render_grid(self, draw):
+        colors = [(100, 100, 100), (200, 100, 0), (150, 150, 150), (0, 100, 200), (255, 255, 0)]
+        x_base = 10
+        y_base = 5
+
+        y_step, y_max = self._get_y_step_max(y_base)
+        for y in range(y_step, y_max, y_step):
+            color = colors[0]
+            if   y % (10 * y_step) == 0: color = colors[1]
+            elif y % ( 5 * y_step) == 0: color = colors[2]
+            if self._loga(y_base, y) >= 1 and color == colors[0]:
+                continue
+            h = np.power(y / y_max, self.y_factor) * self.height
+            draw.line((0, self.height - h, self.width, self.height - h), color, 1)
+
+        x_step, x_max = self._get_x_step_max(x_base)
+        for x in range(-x_max, x_max, x_step):
+
+            color = colors[0]
+            if x == 0: color = colors[4]
+            else:
+                if abs(x_max - x) % (10 * x_step) == 0: color = colors[2]
+                if abs(x_max - x) % ( 5 * x_step) == 0: color = colors[1]
+
+            px = int((x + self.ox) * self.scl)
+            if px < 0 or px > self.width: continue
+            w = 1 if np.abs(px) > 0.1 else 3
+            draw.line((px, self.height, px, 0), color, w)
+
+    def render(self):
+        img = Image.new('RGB', (self.width, self.height), (0, 0, 0))
+        draw = ImageDraw.Draw(img)
+
+        self._render_grid(draw)
+        _, y_max = self._get_y_step_max(10)
+        for item in self.items:
+            x = int((item.x + self.ox) * self.scl)
+            h = np.power(item.height / y_max, self.y_factor) * self.height
+            draw.line((x, self.height - h, x, self.height), (item.color), 3)
+
+        draw.rectangle((0, 0, self.width-1, self.height-1), None, (255, 255, 255), 1)
+        return img
+
+    @staticmethod
+    def _pil2pixmap(im):
+        if im.mode == "RGB":
+            r, g, b = im.split()
+            im = Image.merge("RGB", (b, g, r))
+        elif  im.mode == "RGBA":
+            r, g, b, a = im.split()
+            im = Image.merge("RGBA", (b, g, r, a))
+        elif im.mode == "L":
+            im = im.convert("RGBA")
+        im2 = im.convert("RGBA")
+        data = im2.tobytes("raw", "RGBA")
+        qim = QtGui.QImage(data, im.size[0], im.size[1], QtGui.QImage.Format_ARGB32)
+        pixmap = QtGui.QPixmap.fromImage(qim)
+        return pixmap
 
 
 class Histogram(QtWidgets.QWidget):
@@ -24,12 +155,13 @@ class Histogram(QtWidgets.QWidget):
         self.resy = self.config.get('histogram_resy')
         self.hist_size = (self.resx, self.resy)
         self.hist_max = self.config.get('histogram_max')
+        self.hist_y_factor = self.config.get('histogram_y_factor')
         self.color = ColorLine()
         for knot in self.config.get('colors'):
             self.color.add(knot['alpha'], vec3(*knot['color']))
 
         self.layout = QtWidgets.QHBoxLayout(self)
-        self.layout.setContentsMargins(QtCore.QMargins(1, 1, 1, 1))
+        self.layout.setContentsMargins(QtCore.QMargins(0, 0, 0, 0))
         self.setLayout(self.layout)
 
         self.view = None
@@ -43,14 +175,17 @@ class Histogram(QtWidgets.QWidget):
         palette.setColor(QtGui.QPalette.Window, QtGui.QColor('gray'))
         self.setPalette(palette)
         self.setAutoFillBackground(True)
-        self.change_flag = True
-        self.save_projection = None
-        self.save_navigation = None
-        self.save_scene = None
-        self.save_view = None
+
+        self.scene = Scene(self.width(), self.height(), self.hist_y_factor)
+        self.scene.clear()
+        self.label = QtWidgets.QLabel(self)
+        self.layout.addWidget(self.label)
+        
+        self.old_pos = 0.
 
     def set_number(self, number):
         self.hist_max = int(number)
+        self.scene.clear(number)
 
     def set_spacetime(self, spacetime):
         if self.spacetime is not spacetime:
@@ -59,44 +194,24 @@ class Histogram(QtWidgets.QWidget):
         else:
             self.change_flag = False
 
+    def reset(self):
+        img = self.scene.render()
+        self.label.setPixmap(self.scene._pil2pixmap(img))
+        self.show()
+        self.update()
+
     def set_time(self, time):
         self.time = time
-        
-        if not self.view:
-            if not self.parent():
-                scene = Scene(ctx=mgl.create_context(standalone=True, share=False))
-            else:
-                scene = Scene(ctx=self.parent().view.scene.ctx)
-            self.view = View(
-                scene, 
-                projection=Orthographic(), 
-                navigation=Turntable()
-            )
-            self.layout.addWidget(self.view)
-            # self.view.preload()
-        
-        self.bar_height = float(self.hist_max * self.resy) / float(self.resx)
-        self.bar_width = 0.5
-        self.view.scene.displays.clear()
-        objs = self._make_objs()
-        self.view.scene.add(objs)
-        self.view.scene.render(self.view)
-
+        self._make_items()
         if self.change_flag:
-            print(f'hist: ------- number: {self.hist_max}, resx: {self.resx}, resy: {self.resy}, height: {self.bar_height}')
-            self.view.center(center=fvec3(self.hist_max * 0.5, 1, self.bar_height))
-            self.view.look(fvec3(self.hist_max * 0.5, 0, self.bar_height))
-            self.view.navigation.distance = self.bar_height * 2.5
+            self.scene.fit()
+            self.change_flag = False
+        self.reset()
 
-        del objs
-        self.view.show()
-        self.view.update()
-
-    def _make_objs(self):
-        space = self.spacetime.spaces[self.time]
+    def _make_items(self):
+        space = self.spacetime.spaces.spaces[self.time]
 
         dict_objs = {}
-        dict_keys = {}
 
         count = 0
         max = -1
@@ -108,86 +223,89 @@ class Histogram(QtWidgets.QWidget):
                     max = num
                 if num not in dict_objs:
                     dict_objs[num] = 0
-                dict_objs[num] += num
+                dict_objs[num] += 1
                 count += 1
 
         print(f'hist: ------- time: {self.time}, count: {count}, max: {max}, len: {len(dict_objs)}')
 
+        self.scene.clear()
         for num in dict_objs.keys():
             alpha = float(num) / float(max)
             pos = float(num)
-            obj = brick(
-                center=vec3(pos + self.bar_width * 0.5, self.bar_width * 0.5, self.bar_height), 
-                width=vec3(self.bar_width, self.bar_width, self.bar_height * 2)
-            )
-            obj.option(color=self.color.getColor(alpha))
-            dict_keys[self.config.getKey()] = obj
+            color = self.color.getColor(alpha)
+            # height = float(dict_objs[num]) / float(count)
+            height = float(dict_objs[num])
+            self.scene.add(pos, height, color)
 
-        print(f"hist: ------- key: {self.config.values['objects_key']}")
-
-        return dict_keys
-    
     def prepare_save(self, ctx=None):
-        self.save_projection = deepcopy(self.view.projection)
-        self.save_navigation = deepcopy(self.view.navigation)
-        self.save_scene = Scene(options=None)
-        self.save_view = RenderView(
-            self.save_scene,
-            projection=self.save_projection, 
-            navigation=self.save_navigation,
-            ctx=ctx
-        )
-        self.save_view.resize((self.resx, self.resy))
+        self.old_time = self.time
 
     def save_image(self, time):
-        self.old_time = self.time
         self.time = time
-        self.save_scene.displays.clear()
-        objs = self._make_objs()
-        self.save_scene.add(objs)
-        img = self.save_view.render()
-        self.save_scene.displays.clear()
-        self.time = self.old_time
-        del objs
-        return img
-    
+        self._make_items()
+        img = self.scene.render()
+        return img.convert('RGBA')
+
     def end_save(self):
-        del self.save_projection
-        del self.save_navigation
-        del self.save_scene
-        del self.save_view
-        self.save_projection = None
-        self.save_navigation = None
-        self.save_scene = None
-        self.save_view = None
+        self.time = self.old_time
+        self._make_items()
+
+    def mousePressEvent(self, a0: QMouseEvent) -> None:
+        self.old_pos = float(a0.pos().x())
+        a0.accept()
+
+    def mouseMoveEvent(self, a0: QMouseEvent) -> None:
+        pos = float(a0.pos().x())
+        self.scene.translate(float(pos - self.old_pos))
+        self.old_pos = pos
+        self.reset()
+        a0.accept()
+
+    def wheelEvent(self, a0: QWheelEvent) -> None:
+        pos = float(a0.pos().x())
+        step = float(a0.angleDelta().y() / 120.)
+        self.scene.scale(pos, step)
+        self.reset()
+        a0.accept()
+
+    def keyPressEvent(self, a0: QKeyEvent) -> None:
+        print('hist: ------- key press event...')
+        if a0.key() == QtCore.Qt.Key.Key_F:
+            self.scene.fit()
+            self.reset()
+            a0.accept()
+            return
+        if not self.parent():
+            if a0.key() == QtCore.Qt.Key.Key_Left and self.time > 0:
+                self.set_time(self.time - 1)
+                a0.accept()
+                return
+            elif a0.key() == QtCore.Qt.Key.Key_Right and self.time < 30:
+                self.set_time(self.time + 1)
+                a0.accept()
+                return
+            return super().keyPressEvent(a0)
+
 
 if __name__ == '__main__':
     import sys
     from spacetime import SpaceTime
 
-    opengl_version = (3,3)
+    n = 32769
+    T = 10
+    max = 30
+    dim = 3
 
-    settings_file = r'settings.txt'
-
-    spacetime = SpaceTime(T=8, max=24, dim=3)
-    spacetime.setRationalSet(n=99)
+    spacetime = SpaceTime(T=T, max=max, dim=dim)
+    spacetime.setRationalSet(n)
     spacetime.addRationalSet()
 
-    settings.load(settings_file)
-    
     app = QtWidgets.QApplication(sys.argv)
 
     histogram = Histogram()
-    histogram.set_number(99)
+    histogram.set_number(n)
     histogram.set_spacetime(spacetime)
-    histogram.set_time(24)
-    histogram.view.update()
-    print('set_time...')
+    histogram.set_time(max)
     histogram.show()
-    print('show...')
 
-    err = app.exec()
-    print('exec...')
-    print(f'Qt error = {err}')
-
-    sys.exit(err)
+    sys.exit(app.exec())
